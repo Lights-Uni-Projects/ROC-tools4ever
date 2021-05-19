685baab1b03dea17deff3353545239aee0ba24be
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2021-05-19 08:54:42 +0200

    Add links to other pages

--- src/routes/admin/$layout.svelte
+++ src/routes/admin/$layout.svelte
@@ -13,4 +13,10 @@
 	}
 </script>
 
+<div class="container mx-auto">
+	<div class="center float m-auto relative">
+		<a class="absolute right-20 -top-24" href="/products">Producten</a><p class="absolute right-16 -top-24"> |</p>
+    </div>
+</div>
+
 <slot />
--- src/routes/products/$layout.svelte
+++ src/routes/products/$layout.svelte
@@ -13,4 +13,10 @@
 	}
 </script>
 
+<div class="container mx-auto">
+	<div class="center float m-auto relative">
+		<a class="absolute right-20 -top-24" href="/admin/register">Registreer</a><p class="absolute right-16 -top-24"> |</p>
+    </div>
+</div>
+
 <slot />
